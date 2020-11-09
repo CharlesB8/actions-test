9a44f79af072fe508a0efdc3ed061aaf87646a7d
Charles Bartels <charlieb2898@gmail.com>
2020-11-09 10:05:57 -0600

    added a test

---
+++ src/test/java/AnotherTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AnotherTest {
+
+    @Test
+    public void hello(){
+        fail("no hello");
+    }
+}
